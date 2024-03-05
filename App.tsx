dd51dc3b74f4c896f241bf72317c326512ea349d
facialjun <facialjun@gmail.com>
2024-03-05 13:26:39 +0900

    amplify

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import {NativeBaseProvider} from 'native-base'
 import { Amplify } from 'aws-amplify';
-// import amplifyconfig from './src/aws-exports';
+import amplifyconfig from './src/aws-exports';
 import Navigator from './stacks/Navigator';
 import 'react-native-gesture-handler';
 
 
 
-// Amplify.configure(amplifyconfig);
+Amplify.configure(amplifyconfig);
 
 const App: React.FC = () => {
 
